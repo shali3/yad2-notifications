a5782e1a54f8f3df9c634057c2055ac843d73409
ShaLi Shaltiel <shali3@gmail.com>
2014-07-23 19:57:20 +0300

    Added the Red/Blue functionality like Yad2+ extension.

--- extention/contentscript.js
+++ extention/contentscript.js
@@ -1,74 +1,92 @@
 /**
  * Created by shali on 20/07/14.
  */
+const PROP_VISITED = "visited";
+const PROP_FIRST_SEEN = "firstSeen";
+const NEW_AD_SEC = 60;
+const ADS_SELECTOR = "tr[id^='tr_Ad']";
+const INNER_INTEGRATION_SELECTOR = "td:nth-child(2)";
+
 
 function getAdId($item) {
     var id = $item.attr('id');
-    id = id.substr(id.lastIndexOf('_') + 1);
     return id;
 }
 
-function getShouldFilter(adId) {
-    return localStorage.getItem(adId + '.filter');
+function getProperty(adId, propName) {
+    return localStorage.getItem(adId + '.' + propName);
 }
 
-function setShouldFilter(adId, val) {
-    localStorage.setItem(adId + '.filter', val)
+function wasTouchedSince(adId, propName, secondsAgo) {
+    var str = getProperty(adId, propName);
+    if (str) {
+        var num = parseInt(str);
+        var delta = (new Date()).getTime() - num;
+        return delta < secondsAgo * 1000
+    }
+    return false;
 }
 
-function onRemoveClicked(ev) {
-    ev.preventDefault();
-    $target = $(ev.target);
-    var id = $target.data("id");
-    setShouldFilter(id, true);
-    invalidateFiltered();
+
+function setProperty(adId, propName, val) {
+    localStorage.setItem(adId + '.' + propName, val)
 }
 
-function invalidateFiltered() {
+function touchProperty(adId, propName) {
+    setProperty(adId, propName, (new Date()).getTime());
+}
+
+function invalidateColors() {
     var count = 0;
+    var hasAds = false;
 
-    $("tr[id^='tr_Ad_1_1']").each(function (i, item) {
-        var $item = $(item);
-        var id = getAdId($item);
-        if (getShouldFilter(id)) {
-            $item.hide();
+    $(ADS_SELECTOR).each(function (i, ad) {
+        var $ad = $(ad);
+        var id = getAdId($ad);
+        hasAds = true;
+        if (getProperty(id, PROP_VISITED)) {
+            $ad.find(INNER_INTEGRATION_SELECTOR).addClass('visitedItem');
+        }
+        else if (wasTouchedSince(id, PROP_FIRST_SEEN, NEW_AD_SEC)) {
+            $ad.find(INNER_INTEGRATION_SELECTOR).addClass('newItem');
             count++
         }
-        else {
-            $item.show();
+    });
+
+    if (hasAds) {
+        document.title = '(' + count + ')';
+        //chrome.runtime.sendMessage({msg: "setBadge", number: count});
+    }
+}
+
+
+function onAdClick() {
+    var $ad = $(this);
+    var id = getAdId($ad);
+
+    touchProperty(id, PROP_VISITED);
+    $ad.find(INNER_INTEGRATION_SELECTOR).addClass("visitedItem")
+}
+function init() {
+    var $ads = $(ADS_SELECTOR);
+    $ads.click(onAdClick);
+
+    $ads.each(function (i, ad) {
+        var $ad = $(ad);
+        var id = getAdId($ad);
+        if (!getProperty(id, PROP_FIRST_SEEN)) {
+            touchProperty(id, PROP_FIRST_SEEN);
         }
     });
 
-    var text = '';
-    if (count > 0) {
-        text = 'מתוכם סוננו ' + count;
-    }
-
-    $('.filteredCount').text(text);
+    invalidateColors()
 }
 
-
-function init() {
-    $("tr[id^='tr_Ad_1_1']").each(function (i, item) {
-        var $item = $(item);
-        var id = getAdId($item);
-
-        var removeLink = $('<a href="#"></a>');
-        removeLink.text('הסר');
-        removeLink.addClass('removeLink');
-        removeLink.data('id', id);
-        removeLink.click(onRemoveClicked);
-
-        $item.find("td:nth-child(2)").append(removeLink);
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.msg == "clearFiltered") {
+            clearFiltered()
+        }
     });
-
-
-    var filterCountMessage = $('<i></i>');
-    filterCountMessage.addClass('filteredCount');
-    filterCountMessage.css('font-size', 'small').css('color', 'blue');
-    $('.show_results_from h2').append(filterCountMessage)
-
-    invalidateFiltered();
-}
 
 init();
